2741c42b90a3a86d0fe018ef9747ee378c677654
maggie388 <rodriguesmargarida@hotmail.com>
2021-04-23 11:51:57 -0400

    Refactor MaxHeap so that it extends the Heap class

--- data-structures/max-heap.js
+++ data-structures/max-heap.js
@@ -1,15 +1,9 @@
+const Heap = require('./heap');
 const { getParent, getLeft, getRight } = require('./heap-helpers');
 
-class MaxHeap {
+class MaxHeap extends Heap {
     constructor() {
-        this.heap = [null];
-        this.size = 0;
-    }
-
-    add(value) {
-        this.heap.push(value);
-        this.size++;
-        this.bubbleUp();
+        super()
     }
 
     bubbleUp() {
@@ -19,15 +13,6 @@ class MaxHeap {
             this.swap(currentIndex, parentIndex);
             currentIndex = parentIndex;
         }
-    }
-
-    popMax() {
-        if (!this.size) return null;
-        this.swap(1, this.size);
-        const max = this.heap.pop();
-        this.size--;
-        this.heapify();
-        return max;
     }
 
     heapify() {
@@ -50,21 +35,11 @@ class MaxHeap {
     }
 
     // HELPER METHODS
-    exists(index) {
-        return index <= this.size;
-    }
-
     canSwap(currentIndex, leftChildIndex, rightChildIndex) {
         return (
           this.exists(leftChildIndex) && this.heap[currentIndex] < this.heap[leftChildIndex]
           || this.exists(rightChildIndex) && this.heap[currentIndex] < this.heap[rightChildIndex]
         );
-    }
-
-    swap(a, b) {
-        // this is destructuring assignment syntax
-        // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Destructuring_assignment
-        [this.heap[a], this.heap[b]] = [this.heap[b], this.heap[a]];
     }
 }
 
