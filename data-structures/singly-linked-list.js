560c28777843b646bc729dd8ef87ef652c046841
maggie388 <rodriguesmargarida@hotmail.com>
2021-04-23 11:22:05 -0400

    Move the Node class into a seperate module

--- data-structures/singly-linked-list.js
+++ data-structures/singly-linked-list.js
@@ -1,23 +1,6 @@
-// a linked list is make up of nodes that hold some data 
+const Node = require('./node');
+// a linked list is made up of nodes that hold some data 
 // and point to the next node on the list
-class Node {
-    constructor(data) {
-        this.data = data;
-        this.next = null;
-    }
-
-    setNextNode(node) {
-        if (node instanceof Node || node === null) {
-            this.next = node;
-        } else {
-            throw new Error(`Next node must be a member of the Node class`);
-        }
-    }
-
-    getNextNode() {
-        return this.next;
-    }
-}
 
 class LinkedList {
     constructor() {
